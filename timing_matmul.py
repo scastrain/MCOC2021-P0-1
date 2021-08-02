4ff5d169ea23faa5a912a168982d105584fb1332
scastrain <88336928+scastrain@users.noreply.github.com>
2021-08-02 18:57:23 -0400

    Add files via upload

--- timing_matmul.py
+++ timing_matmul.py
@@ -1,10 +1,9 @@
+
 from numpy import zeros
 from time import perf_counter
 
-
-
 #Tamaño
-N = 1000
+N = 2000
 A = zeros((N, N))+1
 B = zeros((N, N))+2
 
@@ -13,8 +12,6 @@ t1 = perf_counter()
 C = A@B
 t2 = perf_counter()
 
-
 dt = t2 - t1
 
 print(f"dt = {dt} s")
-
